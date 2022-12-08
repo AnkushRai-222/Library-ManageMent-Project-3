b34c144f34008688157231422c2d3a5bf45f7cc1
AnkushRai-222 <ankushrai222@gmail.com>
2022-12-08 22:38:24 +0530

    UserController Done

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -64,11 +64,11 @@ const createUser = async function(req,res){
             if (!valid(address.street)) {
                 return res.status(400).send({ status: false, message: "Street address cannot be empty  ." })
             }
-            if(!ValidName(address.street)){
-                return res.status(400).send({status:false,message:"Street is only accepted in alphabet"})
-            }
             if (!valid(address.city)) {
                 return res.status(400).send({ status: false, message: "City cannot be empty  ." })
+            }
+            if(!ValidName(address.city)){
+                return res.status(400).send({status:false,message:"city is only accepted in alphabet"})
             }
             if (!valid(address.pincode)) {
                 return res.status(400).send({ status: false, message: "Pincode cannot be empty." })
